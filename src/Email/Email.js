20e4f77bbb8ae0b19951364d329c642b68db890d
harshit107 <harshit107.in@gmail.com>
2023-01-20 16:11:22 +0530

    First commit

---
+++ src/Email/Email.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+const appname = 'LetsChat'
+
+const sendMail = async function (email, message, preHtml) {
+
+    const fromEmail =process.env.SENDINBLUE_EMAIL;
+    const password = process.env.SENDINBLUE_PASSWORD;
+ 
+    
+    message = 'This is just a testing mail from Auto Email'
+    let mailTransporter = nodemailer.createTransport({ 
+        host: 'smtp-relay.sendinblue.com',
+        port: 587,
+        auth: { 
+            user: fromEmail, 
+            pass: password
+        } 
+    }); 
+      
+
+    let mailDetails = {
+        from: "noreply@imharshit.tech",
+        to: email,
+        subject: `${appname}`,
+        text: `${message}`,
+        html: preHtml
+    };
+
+    mailTransporter.sendMail(mailDetails, function (err, data) {
+        if (err) {
+            console.log('Error Occurs : '+err);
+        } else {
+            console.log('Email sent successfully');
+        }
+    });
+
+    
+
+}
+module.exports = sendMail
