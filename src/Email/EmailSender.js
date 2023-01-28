68433297ac123b637461ee4e23783aca38aa317d
harshit107 <harshit107.in@gmail.com>
2023-01-28 22:55:58 +0530

    add email and templete

--- src/Email/EmailSender.js
+++ src/Email/EmailSender.js
@@ -3,38 +3,42 @@ const nodemailer = require("nodemailer");
 require('dotenv').config()
 
 
-const sendMail = async function (appName,senderEmail, message, HTMLfile) {
+const sendMail = async function (appName,subject,senderEmail, message, HTMLfile) {
 
-    const fromEmail =process.env.SENDINBLUE_EMAIL;
-    const password = process.env.SENDINBLUE_PASSWORD;
+    const authEmail =process.env.SENDINBLUE_EMAIL;
+    const authPass = process.env.SENDINBLUE_PASSWORD;
  
-    appName = appName || Notification
-    message = message || 'This is just a notification'
     let mailTransporter = nodemailer.createTransport({ 
         host: 'smtp-relay.sendinblue.com',
         port: 587,
         auth: { 
-            user: fromEmail, 
-            pass: password
+            user: authEmail, 
+            pass: authPass
         } 
     }); 
       
 
     let mailDetails = {
-        from: `${appName}@imharshit.tech`,
+        from: `${appName}@donot-Reply.online`,
         to: senderEmail,
-        subject: `${appname}`,
+        subject: `${subject}`,
         text: `${message}`,
         html: HTMLfile
     };
 
-    mailTransporter.sendMail(mailDetails, function (err, data) {
+    const info =  await mailTransporter.sendMail(mailDetails, function (err, data) {
         if (err) {
             console.log('Error Occurs : '+err);
+            return "error"
         } else {
             console.log('Email sent successfully');
+            return "success"
         }
+        // console.log("Data : "+data);
     });
 
+   
+
 }
-module.exports = sendMail
+ module.exports = sendMail
+
--- src/router/PublicEmailSenderRouter.js
+++ src/router/PublicEmailSenderRouter.js
@@ -1,17 +1,45 @@
+const e = require('express');
 const express = require('express');
 const router = new express.Router();
 const emailSender = require('../Email/EmailSender');
+const otpVerificationAsString = require('../HTMLtemplets/otpVerification');
 
 
 
+router.post('/public/email/notification', async (req, res) => {
 
-router.get('/public/email/notification', async (req, res) => {
+    const reqEmailBody = req.body;
+    const senderEmail = reqEmailBody.email; //may be single email or array of Email
+    const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
+    const subject = reqEmailBody.subject;   // Credit Notification
+    const emailContent = reqEmailBody.emailContent; // your orignal Content
+    const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
+    
 
-    await emailSender('harshit107.in@gmail.com',"Hey This is a tesing message",null);
+    await emailSender(appName,subject,senderEmail,emailContent,HTMLfile);
 
-    res.send("<H2> Successful </H2> ");
+    res.status(200).send({"message" : "Success"});
 
 
+})
+
+router.post('/public/email/otpverification', async (req, res) => {
+
+    const reqEmailBody = req.body;
+    const senderEmail = reqEmailBody.email; //may be single email or array of Email
+    const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
+    const subject = reqEmailBody.subject;   // Credit Notification
+    const otp = reqEmailBody.otp; // your orignal Content
+    const withValidTime = reqEmailBody.time; // Single HTML as string    
+    var otpValidString = otpVerificationAsString(appName, otp, withValidTime);
+
+
+    await emailSender(appName, subject, senderEmail, otpValidString, otpValidString);
+    
+    res.status(200).send({"message" : "Success"});
+
+
+   
 })
 
 module.exports = router;
