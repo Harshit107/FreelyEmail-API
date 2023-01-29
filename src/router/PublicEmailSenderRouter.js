474ad6adc8ef2706a763e98e12a9da3a5ad53c49
harshit107 <harshit107.in@gmail.com>
2023-01-29 23:28:44 +0530

    remove log

--- src/router/PublicEmailSenderRouter.js
+++ src/router/PublicEmailSenderRouter.js
@@ -9,14 +9,17 @@ const otpVerificationAsString = require('../HTMLtemplets/otpVerification');
 router.post('/public/email/notification', async (req, res) => {
 
     const reqEmailBody = req.body;
-    const senderEmail = reqEmailBody.email; //may be single email or array of Email
+
+    const senderEmail = reqEmailBody.senderEmail; //may be single email or array of Email
+    const recipientsEmail = reqEmailBody.recipientsEmail; //may be single email or array of Email
     const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
     const subject = reqEmailBody.subject;   // Credit Notification
     const emailContent = reqEmailBody.emailContent; // your orignal Content
     const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
-    
+    // console.log('senderEmail :>> ', reqEmailBody);
 
-    await emailSender(appName,subject,senderEmail,emailContent,HTMLfile);
+
+    await emailSender(appName,subject,recipientsEmail,senderEmail,emailContent,HTMLfile);
 
     res.status(200).send({"message" : "Success"});
 
@@ -26,15 +29,15 @@ router.post('/public/email/notification', async (req, res) => {
 router.post('/public/email/otpverification', async (req, res) => {
 
     const reqEmailBody = req.body;
-    const senderEmail = reqEmailBody.email; //may be single email or array of Email
+    const senderEmail = reqEmailBody.senderEmail; //may be single email or array of Email
+    const recipientsEmail = reqEmailBody.recipientsEmail; //may be single email or array of Email
     const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
     const subject = reqEmailBody.subject;   // Credit Notification
     const otp = reqEmailBody.otp; // your orignal Content
     const withValidTime = reqEmailBody.time; // Single HTML as string    
     var otpValidString = otpVerificationAsString(appName, otp, withValidTime);
 
-
-    await emailSender(appName, subject, senderEmail, otpValidString, otpValidString);
+    await emailSender(appName, subject,recipientsEmail, senderEmail, otpValidString, otpValidString);
     
     res.status(200).send({"message" : "Success"});
 
