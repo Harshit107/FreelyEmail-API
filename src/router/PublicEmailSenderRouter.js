c94ba87861c15f29e887c959178862aeacfccda2
harshit107 <harshit107.in@gmail.com>
2023-02-11 20:35:54 +0530

    Es6 feature and Validation

--- src/router/PublicEmailSenderRouter.js
+++ src/router/PublicEmailSenderRouter.js
@@ -19,43 +19,51 @@ function sendError (res, errorMessage) {
     })
 }
 
+function validateRequest (req, res) {
+    if(!validateData(req.senderEmail) ){
+        
+        sendError(res,"Enter Valid Sender Email Address");
+        return false;
+    }
+
+    if(!validateData(req.recipientsEmail) ){
+        sendError(res,"Enter Valid recipient Email Address");
+        return false;
+    }
+
+    if(!validateData(req.appName) ){
+        sendError(res,"Enter Valid App Name");
+        return false;;
+    }
+
+    if(!validateData(req.subject) ){
+        sendError(res,"Enter Valid Subject");
+        return false;;
+    }
+    
+    return true;
+}
+
 
 router.post('/public/email/notification', async (req, res) => {
 
     const reqEmailBody = req.body;
 
-    const senderEmail = reqEmailBody.senderEmail; //may be single email or array of Email
-    const recipientsEmail = reqEmailBody.recipientsEmail; //may be single email or array of Email
-    const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
-    const subject = reqEmailBody.subject;   // Credit Notification
-    const emailContent = reqEmailBody.emailContent; // your orignal Content
-    const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
+    const {senderEmail,recipientsEmail,appName,subject,emailContent,HTMLfile} = reqEmailBody; //may be single email or array of Email
+    
+    // const recipientsEmail = reqEmailBody.recipientsEmail; //may be single email or array of Email
+    // const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
+    // const subject = reqEmailBody.subject;   // Credit Notification
+    // const emailContent = reqEmailBody.emailContent; // your orignal Content
+    // const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
 
-    if(!validateData(senderEmail) ){
-        
-        sendError(res,"Enter Valid Sender Email Address");
+    if(!validateRequest(reqEmailBody,res))
         return;
-    }
-
-    if(!validateData(recipientsEmail) ){
-        sendError(res,"Enter Valid recipient Email Address");
-        return;
-    }
-
-    if(!validateData(appName) ){
-        sendError(res,"Enter Valid App Name");
-        return;
-    }
-
-    if(!validateData(subject) ){
-        sendError(res,"Enter Valid Subject");
-        return;
-    }
+   
     if(!validateData(emailContent) && !validateData(HTMLfile) ){
         sendError(res,"Enter Valid Email Content");
-        return;
+        return false;;
     }
-
     
     try{
        const msg = await emailSender(appName,subject,recipientsEmail,senderEmail,emailContent,HTMLfile);
@@ -77,28 +85,42 @@ router.post('/public/email/notification', async (req, res) => {
 
 })
 
+
+// ---------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------
+
+
 router.post('/public/email/otpverification', async (req, res) => {
 
     const reqEmailBody = req.body;
-    const senderEmail = reqEmailBody.senderEmail; //may be single email or array of Email
-    const recipientsEmail = reqEmailBody.recipientsEmail; //may be single email or array of Email
-    const appName = reqEmailBody.appName;   //eg : google@donot-reply.online
-    const subject = reqEmailBody.subject;   // Credit Notification
-    const otp = reqEmailBody.otp; // your orignal Content
-    const withValidTime = reqEmailBody.time; // Single HTML as string    
-    var otpValidString = otpVerificationAsString(appName, otp, withValidTime);
+    
+    const {senderEmail,recipientsEmail,appName,subject,otp,withValidTime} = reqEmailBody; //may be single email or array of Email
+       
+
+    if(!validateRequest(reqEmailBody,res))
+        return;
+
+    if(!validateData(otp) ){
+        sendError(res,"Enter Valid OTP");
+        return false;;
+    }
+
+    var otpValidString = otpVerificationAsString(appName, otp, withValidTime) ;
+    
 
     try{
-        const msg = await emailSender(appName,subject,recipientsEmail,senderEmail,emailContent,HTMLfile);
+        const msg = await emailSender(appName,subject,recipientsEmail,senderEmail,otp,otpValidString);
+        
          res.status(200).send({
              "data" : msg.messageId,
              "error" : {}
          });
      }
      catch(e){
+        console.log(e);
          res.status(400).send({
              "data" : {},
-             "error" : e.response
+             "error" : e.response || "Error Occured, Check your Input"
          });
      }
 
