f7b9ae068460c7fd8ae27004edc017f0470b4a23
harshit107 <harshit107.in@gmail.com>
2023-02-11 19:58:39 +0530

    minnor bug Change

--- src/Email/EmailSender.js
+++ src/Email/EmailSender.js
@@ -28,17 +28,9 @@ const sendMail = async function (appName,subject,recipientsEmail,senderEmail, me
         html: HTMLfile
     };
 
-    const info =  await mailTransporter.sendMail(mailDetails, function (err, data) {
-        if (err) {
-            console.log('Error Occurs : '+err);
-            return "error"
-        } else {
-            console.log('Email sent successfully');
-            return "success"
-        }
-        // console.log("Data : "+data);
-    });
-
+    const info =  await mailTransporter.sendMail(mailDetails);
+    // console.log('info :>> ', info);
+    return info;
    
 
 }
