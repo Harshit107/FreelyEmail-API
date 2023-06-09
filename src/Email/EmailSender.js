1046baebe39f72202f28e89a8befb1b9c370c45c
harshit107 <harshit107.in@gmail.com>
2023-06-09 23:35:38 +0530

    repltTo Option added

--- src/Email/EmailSender.js
+++ src/Email/EmailSender.js
@@ -3,32 +3,38 @@ const nodemailer = require("nodemailer");
 require('dotenv').config()
 
 
-const sendMail = async function (app,subject,recipient,sender, message, HTMLfile) {
+const sendMail = async function (
+  app,
+  subject,
+  recipient,
+  sender,
+  replyTo,
+  message,
+  HTMLfile
+) {
+  //after changing this please review domain
+  const authEmail = process.env.SENDINBLUE_EMAIL;
+  const authPass = process.env.SENDINBLUE_PASSWORD;
 
-    //after changing this please review domain
-    const authEmail =process.env.SENDINBLUE_EMAIL;
-    const authPass = process.env.SENDINBLUE_PASSWORD;
- 
-    let mailTransporter = nodemailer.createTransport({ 
-        host: 'smtp-relay.sendinblue.com',
-        port: 587,
-        auth: { 
-            user: authEmail, 
-            pass: authPass
-        } 
-    }); 
-      
+  let mailTransporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    auth: {
+      user: authEmail,
+      pass: authPass,
+    },
+  });
 
-    let mailDetails = {
-        from: `"${app}" <${sender}@donot-Reply.online>`,
-        to: recipient,
-        subject: `${subject}`,
-        text: `${message}`,
-        html: HTMLfile
-    };
-    const info =  await mailTransporter.sendMail(mailDetails);
-    return info.messageId;
-   
-}
+  let mailDetails = {
+    from: `"${app}" <${sender}@donot-Reply.online>`,
+    to: recipient,
+    replyTo: `${replyTo}`,
+    subject: `${subject}`,
+    text: `${message}`,
+    html: HTMLfile,
+  };
+  const info = await mailTransporter.sendMail(mailDetails);
+  return info.messageId;
+};
  module.exports = sendMail
 
--- src/router/PublicEmailSenderRouter.js
+++ src/router/PublicEmailSenderRouter.js
@@ -1,140 +1,138 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const emailSender = require('../Email/EmailSender');
-const otpVerificationAsString = require('../HTMLtemplets/otpVerification');
+const emailSender = require("../Email/EmailSender");
+const otpVerificationAsString = require("../HTMLtemplets/otpVerification");
 const { validateData, validateRequest } = require("../Validator");
 
-
-function sendError (res, errorMessage) {
-    console.log('errorMessage :>> ', errorMessage);
-    res.status(400).send({
-        "data" : {},
-        "error" : errorMessage
-    })
+function sendError(res, errorMessage) {
+  console.log("errorMessage :>> ", errorMessage);
+  res.status(400).send({
+    data: {},
+    error: errorMessage,
+  });
 }
-
 
 // /---------------    Simple Email  -------------------------------/
 
-router.post('/public/email/notification', async (req, res) => {
+router.post("/public/email/notification", async (req, res) => {
+  const reqEmailBody = req.body;
+  const { sender, recipient, replyTo, app, subject, message, HTMLfile } =
+    reqEmailBody; //may be single email or array of Email
 
-    const reqEmailBody = req.body;
-    const {sender,recipient,app,subject,message,HTMLfile} = reqEmailBody; //may be single email or array of Email
-    
-    // const recipient = reqEmailBody.recipient; //may be single email or array of Email
-    // const app = reqEmailBody.app;   //eg : google@donot-reply.online
-    // const subject = reqEmailBody.subject;   // Credit Notification
-    // const message = reqEmailBody.message; // your orignal Content
-    // const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
+  // const recipient = reqEmailBody.recipient; //may be single email or array of Email
+  // const app = reqEmailBody.app;   //eg : google@donot-reply.online
+  // const subject = reqEmailBody.subject;   // Credit Notification
+  // const message = reqEmailBody.message; // your orignal Content
+  // const HTMLfile = reqEmailBody.HTMLfile; // Single HTML as string
 
-    if(!validateRequest(reqEmailBody,res))
-        return;
-   
-    if(!validateData(message) && !validateData(HTMLfile) ){
-        sendError(res,"Enter Valid Email Content");
-        return false;;
-    }
-    
-    try{
-       const msg = await emailSender(app,subject,recipient,sender,message,HTMLfile);
-        res.status(200).send({
-            "data" : {msg},
-            "error" : {}
-        });
-    }
-    catch(e){
-        res.status(400).send({
-            "data" : {},
-            "error" : e.response
-        });
-    }
-    
-})
+  if (!validateRequest(reqEmailBody, res)) return;
 
+  if (!validateData(message) && !validateData(HTMLfile)) {
+    sendError(res, "Enter Valid Email Content");
+    return false;
+  }
+
+  try {
+    const msg = await emailSender(
+      app,
+      subject,
+      recipient,
+      sender,
+      replyTo,
+      message,
+      HTMLfile
+    );
+    res.status(200).send({
+      data: { msg },
+      error: {},
+    });
+  } catch (e) {
+    res.status(400).send({
+      data: {},
+      error: e.response,
+    });
+  }
+});
 
 // ---------------------       Email OTP With Templete      --------------------------------------------------
 
-router.post('/public/email/otp', async (req, res) => {
+router.post("/public/email/otp", async (req, res) => {
+  const reqEmailBody = req.body;
+  const { sender, recipient,replyTo, app, subject, otp, withValidTime, HTMLfile } =
+    reqEmailBody; //may be single email or array of Email
 
-    const reqEmailBody = req.body;
-    const {sender,recipient,app,subject,otp,withValidTime, HTMLfile} = reqEmailBody; //may be single email or array of Email
+  if (!validateRequest(reqEmailBody, res)) return;
 
-    if(!validateRequest(reqEmailBody,res))
-        return;
+  if (!validateData(otp)) {
+    sendError(res, "Enter Valid OTP");
+    return false;
+  }
+  var HTMLtemplete =
+    HTMLfile || otpVerificationAsString(app, otp, withValidTime);
 
-    if(!validateData(otp) ){
-        sendError(res,"Enter Valid OTP");
-        return false;;
-    }
-    var HTMLtemplete = HTMLfile || otpVerificationAsString(app, otp, withValidTime) ;
-
-    try{
-        const msg = await emailSender(
-          app,
-          subject,
-          recipient,
-          sender,
-          otp,
-          HTMLtemplete
-        );  
-         res.status(200).send({
-             "data" : {msg},
-             "error" : {}
-         });
-     }
-     catch(e){
-        console.log(e);
-         res.status(400).send({
-             "data" : {},
-             "error" : e.response || "Error Occured, Check your Input"
-         });
-     }
-
-})
-
+  try {
+    const msg = await emailSender(
+      app,
+      subject,
+      recipient,
+      sender,
+      replyTo,
+      otp,
+      HTMLtemplete
+    );
+    res.status(200).send({
+      data: { msg },
+      error: {},
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({
+      data: {},
+      error: e.response || "Error Occured, Check your Input",
+    });
+  }
+});
 
 // ---------------------       Email Request OTP With Templete      --------------------------------------------------
 
-router.post('/public/email/otp/request', async (req, res) => {
+router.post("/public/email/otp/request", async (req, res) => {
+  const reqEmailBody = req.body;
+  const { sender, recipient, replyTo, app, subject, withValidTime } =
+    reqEmailBody; //may be single email or array of Email
 
-    const reqEmailBody = req.body;
-    const { sender, recipient, app, subject, withValidTime } = reqEmailBody; //may be single email or array of Email
+  if (!validateRequest(reqEmailBody, res)) return;
 
-    if(!validateRequest(reqEmailBody,res))
-        return;
+  let otp = "";
+  for (let i = 0; i < 6; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
 
-    let otp = "";
-    for (let i = 0; i < 6; i++) {
-        otp += Math.floor(Math.random() * 10);
-    }
+  var HTMLtemplete = otpVerificationAsString(app, otp, withValidTime);
 
-    var HTMLtemplete = otpVerificationAsString(app, otp, withValidTime) ;
-
-    try{
-        const msg = await emailSender(
-          app,
-          subject,
-          recipient,
-          sender,
-          otp,
-          HTMLtemplete
-        );  
-         res.status(200).send({
-             "data" : {
-                messageId : msg,
-                otp : otp
-             },
-             "error" : {}
-         });
-     }
-     catch(e){
-        console.log(e);
-         res.status(400).send({
-             "data" : {},
-             "error" : e.response || "Error Occured, Check your Input"
-         });
-     }
-
-})
+  try {
+    const msg = await emailSender(
+      app,
+      subject,
+      recipient,
+      sender,
+      replyTo,
+      otp,
+      HTMLtemplete
+    );
+    res.status(200).send({
+      data: {
+        messageId: msg,
+        otp: otp,
+      },
+      error: {},
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({
+      data: {},
+      error: e.response || "Error Occured, Check your Input",
+    });
+  }
+});
 
 module.exports = router;
