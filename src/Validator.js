c7cb2eca4ec86f46bb1be48b92d56d29e110de1a
harshit107 <harshit107.in@gmail.com>
2023-07-16 16:19:20 +0530

    sendError bug resolve

--- src/Validator.js
+++ src/Validator.js
@@ -20,17 +20,14 @@ function validateRequest(req, res) {
   }
 
   if (!validateData(req.recipient)) {
-    sendError(res, "Enter Valid recipient Email Address");
     return false;
   }
 
   if (!validateData(req.app)) {
-    sendError(res, "Enter Valid App Name");
     return false;
   }
 
   if (!validateData(req.subject)) {
-    sendError(res, "Enter Valid Subject");
     return false;
   }
 
